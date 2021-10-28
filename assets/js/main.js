591330928cecd84101f19e849c004fcd4ad479e2
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-28 15:25:00 +0200

    complete excercise

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const root = new Vue({
+    el: '#root',
+    data: {
+        message: 'Hello new VueJS World!',
+        h1Class: 'testClass',
+        //bonus
+        img: 'http://lorempixel.com/400/200/',
+        imgAlt: 'random image'
+    }
+})
